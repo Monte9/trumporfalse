4850c7f504141a8e93a952f13777d31bcc96f26d
tjclavel <thomasclavelli@Thomass-MacBook-Air-4.local>
2016-08-13 16:26:35 -0700

    added results page and question page

---
+++ trumporfalse/trumporfalse/ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  trumporfalse
+//
+//  Created by Thomas Clavelli on 8/13/16.
+//  Copyright © 2016 Monte Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+}
--- trumporfalse/trumporfalse/ViewController.swift
+++ trumporfalse/trumporfalse/ViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    
+    
+    // MARK: Outlets
+    @IBOutlet weak var quoteBox: UILabel!
+    @IBOutlet weak var streakLabel: UILabel!
+    @IBOutlet weak var countdownTimer: UILabel!
+    
+    
+    var countdown = 10
+    var myTimer: NSTimer? = nil
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        myTimer = NSTimer(timeInterval: 1.0, target: self, selector:#selector(ViewController.countDownTick), userInfo: nil, repeats: true)
+        countdownTimer.text = "\(countdown)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,13 +34,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func trueButtonPressed(sender: AnyObject) {
-        print("TRUE WAS PRESSED")
+    func countDownTick() {
+        print("called countdowntick")
+        countdown -= 1
+        
+        if (countdown == 0) {
+            myTimer!.invalidate()
+            myTimer = nil
+        }
+        
+        countdownTimer.text = "\(countdown)"
     }
     
-    @IBAction func falseButtonPressed(sender: AnyObject) {
-        print("FALSE WAS PRESSED")
+    @IBAction func choseFalse(sender: UIButton) {
+        print("You chose false")
     }
 
+    @IBAction func choseTrue(sender: UIButton) {
+        print("You chose true")
+    }
+    
+    
 }
 
