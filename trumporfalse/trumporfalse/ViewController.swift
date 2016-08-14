ada1921a2cbdb739759df9e6ddafd62753ed40ac
Monte9 <Monte9@users.noreply.github.com>
2016-08-13 21:27:20 -0700

    logic and initial designs

--- trumporfalse/trumporfalse/ViewController.swift
+++ trumporfalse/trumporfalse/ViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 import Realm
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     struct Quote {
         var statement: String
         var valid: Bool
+    }
+    
+    //Store state of the speech Uterrance for pause/play functionality
+    struct TextToSpeech {
+        static var pausing: Bool? = false
+        static var previousIndex: NSIndexPath = NSIndexPath()
     }
     
     // MARK: Outlets
@@ -21,6 +28,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var streakLabel: UILabel!
     @IBOutlet weak var countdownTimer: UILabel!
     @IBOutlet weak var trumpImage: UIImageView!
+    
+    // text-to-speech code
+    let speechSynthesizer = AVSpeechSynthesizer()
+    var rate: Float!
+    var pitch: Float!
+    var volume: Float!
+    
+    
+
+    
     
     var countdown = 10
     var myTimer: NSTimer? = nil
@@ -41,10 +58,20 @@ class ViewController: UIViewController {
 
         
         quotes[0] = Quote(statement: "Donald Trump!", valid: false)
-        quotes[1] = Quote(statement: "YAY!", valid: true)
+        quotes[1] = Quote(statement: "My IQ is one of the highest — and you all know it! Please don't feel so stupid or insecure; it's not your fault.", valid: true)
         quotes[2] = Quote(statement: "BOOM lol", valid: false)
         
         quoteBox.text = quotes[index]?.statement
+        
+        //Text-to-Speech settings
+        if !loadSettings() {
+            registerDefaultSettings()
+        }
+        
+        //playStatement()
+        //playSound("Desiigner")
+        
+        play()
         
         //addQuotesToRealmDB()
         
@@ -86,7 +113,76 @@ class ViewController: UIViewController {
 //
 //        
 //    }
-//    
+//
+    func play() {
+        var player: AVAudioPlayer! = nil
+        
+        do {
+            var targetURL : NSURL? = NSBundle.mainBundle().URLForResource("Checked-528", withExtension: "png")
+            print(targetURL)
+            let path = NSBundle.mainBundle().pathForResource("trumpAudio", ofType:"m4a")
+            let fileURL = NSURL(fileURLWithPath: path!)
+            try player = AVAudioPlayer(contentsOfURL: fileURL)
+            player.prepareToPlay()
+            player.delegate = self
+            player.play()
+        } catch {
+            print("error initializing AVAudioPlayer")
+        }
+    }
+    
+    func playSound(nameOfAudioFileInAssetCatalog: String) {
+        var alarmAudioPlayer: AVAudioPlayer?
+        if let sound = NSDataAsset(name: nameOfAudioFileInAssetCatalog) {
+            do {
+                print("here")
+                try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+                try! AVAudioSession.sharedInstance().setActive(true)
+                try alarmAudioPlayer = AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeMPEGLayer3)
+                alarmAudioPlayer!.play()
+                print(alarmAudioPlayer)
+            } catch {
+                print("error initializing AVAudioPlayer")
+            }
+        }
+    }
+
+    //delegate methods for the ArticleCellDelegate
+    func playStatement() {
+        
+        let text = quotes[index]?.statement
+        
+        let speechUtterance = AVSpeechUtterance(string: text!)
+        speechUtterance.rate = rate
+        speechUtterance.pitchMultiplier = pitch
+        speechUtterance.volume = volume
+        speechSynthesizer.speakUtterance(speechUtterance)
+        
+    }
+    
+    //Text-to-Speech default settings
+    func registerDefaultSettings() {
+        rate = AVSpeechUtteranceDefaultSpeechRate
+        pitch = 1.0
+        volume = 1.0
+        
+        let defaultSpeechSettings: Dictionary<String, AnyObject> = ["rate": rate, "pitch": pitch, "volume": volume]
+        NSUserDefaults.standardUserDefaults().registerDefaults(defaultSpeechSettings)
+    }
+    
+    //load Text-to-Speech default settings
+    func loadSettings() -> Bool {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        if let theRate: Float = userDefaults.valueForKey("rate") as? Float {
+            rate = theRate
+            pitch = userDefaults.valueForKey("pitch") as! Float
+            volume = userDefaults.valueForKey("volume") as! Float
+            return true
+        }
+        return false
+    }
+
+    
     func updateCounter() {
         countdown -= 1
         
@@ -128,18 +224,23 @@ class ViewController: UIViewController {
             self.view.backgroundColor = UIColor.greenColor()
             self.myTimer!.invalidate()
             }) { (true) in
-                self.index += 1
-                self.index = self.index % self.quotes.count
-                self.quoteBox.text = self.quotes[self.index]?.statement
-                self.view.backgroundColor = UIColor.whiteColor()
-                self.countdown = 10
-                self.myTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector:"updateCounter", userInfo: nil, repeats: true)
-                self.countdownTimer.text = "\(self.countdown)"
-                self.streak += 1
-                self.streakLabel.text = String(self.streak)
-                
+                UIView.animateWithDuration(1.0, animations: {
+                    self.playSound("trumpAudio")
+                    }, completion: { (true) in
+                        self.index += 1
+                        self.index = self.index % self.quotes.count
+                        self.quoteBox.text = self.quotes[self.index]?.statement
+                        self.view.backgroundColor = UIColor.whiteColor()
+                        self.countdown = 10
+                        self.myTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector:"updateCounter", userInfo: nil, repeats: true)
+                        self.countdownTimer.text = "\(self.countdown)"
+                        self.streak += 1
+                        self.streakLabel.text = String(self.streak)
+                        self.playStatement()                })
                 
         }
+        
+        
     }
     
     func showLoseScreen() {
@@ -150,11 +251,17 @@ class ViewController: UIViewController {
             self.view.backgroundColor = UIColor.redColor()
             self.myTimer!.invalidate()
         }) { (true) in
-            self.index = 1
-            self.view.backgroundColor = UIColor.whiteColor()
-            self.trumpImage.hidden = false
+            UIView.animateWithDuration(5.0, animations: { 
+                self.index = 1
+                self.view.backgroundColor = UIColor.whiteColor()
+                self.trumpImage.hidden = false
+                }, completion: { (true) in
+                    let start_game_storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let vc: UIViewController = start_game_storyboard.instantiateViewControllerWithIdentifier("startViewController") as UIViewController
+                    self.presentViewController(vc, animated: true, completion: nil)
+            })
         }
-        
+    
     }
     
     
