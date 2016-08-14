f6439292ea66e3e68786a8723b10d89b3a2a4489
tjclavel <thomasclavelli@Thomass-MacBook-Air-4.local>
2016-08-14 10:14:35 -0700

    added more quotes

--- trumporfalse/trumporfalse/QuoteSource.swift
+++ trumporfalse/trumporfalse/QuoteSource.swift
@@ -57,6 +57,28 @@ class QuoteSource: NSObject {
         addEntry("My IQ is one of the highest — and you all know it! Please don't feel so stupid or insecure; it's not your fault.", isvalid: true)
         addEntry("I have so many fabulous friends who happen to be gay, but I am a traditionalist.", isvalid: true)
         addEntry("Number one, I have great respect for women. I was the one that really broke the glass ceiling on behalf of women, more than anybody in the construction industry.", isvalid: true)
+        addEntry("So you know what I'm gonna do? I'm gonna do something really outrageous, I'm gonna tell the truth.", isvalid: false)
+        addEntry("I feel like I'm too busy writing history to read it.", isvalid: false)
+        addEntry("Nobody builds walls better than me.", isvalid: true)
+        addEntry("No, I’m not into anal.", isvalid: true)
+        addEntry("I love the attention but I don't like too much of it.", isvalid: false)
+        addEntry("Everyone thinks I’m such a nice guy.", isvalid: true)
+        addEntry("Who says I am not under the special protection of God?", isvalid: false)
+        addEntry("It’s freezing and snowing in New York – we need global warming!", isvalid: true)
+        addEntry("I still think I am the greatest.", isvalid: false)
+        addEntry("I don't know what's better gettin' laid or gettin' paid.", isvalid: false)
+        addEntry("I will go down as the voice of this generation, of this decade, I will be the loudest voice.", isvalid: false)
+        addEntry("Wealthy people don’t like me.", isvalid: true)
+        addEntry("I am not a fan of books.", isvalid: false)
+        addEntry("I don't hate women - they just sometimes make me mad.", isvalid: false)
+        addEntry("If I ever ran for office, I’d do better as a Democrat than as a Republican", isvalid: true)
+        addEntry("The truth is that men are tired of liberty.", isvalid: false)
+        addEntry("Nobody can tell me where I can and can't go.", isvalid: false)
+        addEntry("Usually if I fire somebody who’s bad, I’ll tell them how great they are. Because I don’t want to hurt people’s feelings.", isvalid: true)
+        addEntry("One of the problems with defending free speech is you often have to defend people that you find to be outrageous and unpleasant and disgusting.", isvalid: false)
+        addEntry("Nothing in life is promised except death.", isvalid: false)
+        addEntry("It is not truth that matters, but victory.", isvalid: false)
+        addEntry("I do not see why man should not be just as cruel as nature.", isvalid: false)
         
         
         randomizeSource()
@@ -70,7 +92,7 @@ class QuoteSource: NSObject {
     }
     
     func randomizeSource() {
-        for i in 1..<numEntries {
+        for i in 0..<numEntries {
             let rand = Int(arc4random_uniform(UInt32(numEntries)))
             let temp = quotes[rand]
             quotes[rand] = quotes[i]
